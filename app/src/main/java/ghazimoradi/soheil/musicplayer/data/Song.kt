967d7fddc0628a66fa2aa9459b247e51e7b13179
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-07-07 18:06:30 +0330

    add description for song data

--- app/src/main/java/ghazimoradi/soheil/musicplayer/data/Song.kt
+++ app/src/main/java/ghazimoradi/soheil/musicplayer/data/Song.kt
@@ -8,6 +8,9 @@ data class Song(
     val id: Long,
     val title: String?,
     val artist: String?,
+    /**
+     * data is the path of the song
+     */
     val data: String,
     val albumId: Long,
 ): Parcelable
