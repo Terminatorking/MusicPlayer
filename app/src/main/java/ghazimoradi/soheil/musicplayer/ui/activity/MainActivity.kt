ba7cf51146ec169247e68e719609ce778bef9560
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-07-08 19:13:32 +0330

    fix status and navigation bar colors

--- app/src/main/java/ghazimoradi/soheil/musicplayer/ui/activity/MainActivity.kt
+++ app/src/main/java/ghazimoradi/soheil/musicplayer/ui/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package ghazimoradi.soheil.musicplayer.ui.activity
 
+import android.graphics.Color.TRANSPARENT
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle.Companion.dark
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,7 +21,10 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = dark(TRANSPARENT),
+            navigationBarStyle = dark(TRANSPARENT)
+        )
         setContent {
             navController = rememberNavController()
             Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
--- app/src/main/java/ghazimoradi/soheil/musicplayer/ui/components/SongListItem.kt
+++ app/src/main/java/ghazimoradi/soheil/musicplayer/ui/components/SongListItem.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -72,6 +73,8 @@ fun SongListItem(song: Song, onClick: () -> Unit) {
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis
             )
+
+            Spacer(Modifier.size(8.dp))
 
             Text(
                 text = song.artist.orEmpty(),
