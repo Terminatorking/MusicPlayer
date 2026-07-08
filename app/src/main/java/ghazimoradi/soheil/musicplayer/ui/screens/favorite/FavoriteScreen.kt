e03be02ba6c380079424d0666b08a2936b61c9aa
Soheil Ghazimoradi <107791627+Terminatorking@users.noreply.github.com>
2026-07-08 21:50:10 +0330

    add search for favorite

--- app/src/main/java/ghazimoradi/soheil/musicplayer/ui/screens/favorite/FavoriteScreen.kt
+++ app/src/main/java/ghazimoradi/soheil/musicplayer/ui/screens/favorite/FavoriteScreen.kt
@@ -1,26 +1,51 @@
 package ghazimoradi.soheil.musicplayer.ui.screens.favorite
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import ghazimoradi.soheil.musicplayer.data.model.Song
+import ghazimoradi.soheil.musicplayer.ui.components.ProjectOutlinedTextField
 import ghazimoradi.soheil.musicplayer.ui.components.SongList
+import ghazimoradi.soheil.musicplayer.ui.theme.White
 import ghazimoradi.soheil.musicplayer.viewmodel.FavoriteViewModel
+import kotlin.text.isEmpty
 
 @Composable
 fun FavoriteScreen(
     viewmodel: FavoriteViewModel = hiltViewModel(),
     navigateToPlayer: (songs: List<Song>, position: Int) -> Unit
 ) {
+    var search by remember { mutableStateOf("") }
 
     val favoriteSongs = remember { mutableStateOf<List<Song>>(emptyList()) }
+
+    val songsState by remember {
+        derivedStateOf {
+            if (search.isEmpty()) {
+                favoriteSongs.value
+            } else {
+                favoriteSongs.value.filter { it.title?.contains(search, ignoreCase = true) == true }
+            }
+        }
+    }
 
     LaunchedEffect(Unit) {
         viewmodel.getFavorites().collect {
@@ -29,14 +54,44 @@ fun FavoriteScreen(
     }
 
     Column(modifier = Modifier.fillMaxSize()) {
-        SongList(
-            songs = favoriteSongs.value,
-            onSongClick = { pos ->
-                navigateToPlayer.invoke(favoriteSongs.value, pos)
-            },
-            modifier = Modifier
-                .weight(1f)
-                .padding(start = 20.dp)
-        )
+
+        if (favoriteSongs.value.isEmpty()) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+            ) {
+
+                Spacer(modifier = Modifier.size(20.dp))
+
+                Text(
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    text = "Favorite list is Empty",
+                    color = White,
+                )
+            }
+        } else {
+
+            ProjectOutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(20.dp),
+                value = search,
+                onValueChange = {
+                    search = it
+                }
+            )
+
+            SongList(
+                songs = songsState,
+                onSongClick = { pos ->
+                    navigateToPlayer.invoke(songsState, pos)
+                },
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(start = 20.dp)
+            )
+        }
     }
 }
